25fd9cd82ec8591cd0296114a1d830d4f5686152
Norbert <kieler103@gmail.com>
2025-05-31 12:11:26 +0200

    Dodaj pliki projektów.

---
+++ BeeProductive/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeeProductive.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
