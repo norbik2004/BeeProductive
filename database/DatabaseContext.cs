f9aaa2d843c7b1fd711d4ac5f9893e6d52977692
Norbert <kieler103@gmail.com>
2025-06-04 20:27:18 +0200

    setup

---
+++ database/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using DatabaseEntities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseEntities
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ database/DatabaseContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DatabaseEntities
+{
+    public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+    {
+        public DatabaseContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+
+            // Tutaj wpisz connection string do Twojej bazy Postgres
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=BeeProductiveDB;Username=postgres;Password=root");
+
+            return new DatabaseContext(optionsBuilder.Options);
+        }
+    }
+}
